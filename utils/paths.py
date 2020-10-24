c6e663a0b19919febdb7001839fc5f4a0a1f9da6
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2020-10-24 11:09:40 -0400

    Update paths.py

--- utils/paths.py
+++ utils/paths.py
@@ -23,6 +23,48 @@ if __name__ == '__main__':
     subprocess.call(['mv', '{0}/temp.config'.format(path),
                            '{0}/gwas.config'.format(path)])
 
+    with open('{0}/configs/temp.config'.format(path), 'w') as outfile:
+        with open('{0}/configs/gwas_1KG_logistic.config'.format(path), 'r') as infile:
+            for line in infile:
+                if line.lstrip().startswith('indir'):
+                    outfile.write('  indir  = "{0}/data"\n'.format(path))
+                elif line.lstrip().startswith('outdir'): 
+                    outfile.write('  outdir = "{0}/result-gwas-1KG-logistic"\n'.format(path))
+                else:
+                    outfile.write(line)
+
+    subprocess.call(['rm', '-rf', '{0}/configs/gwas_1KG_logistic.config'.format(path)])
+    subprocess.call(['mv', '{0}/configs/temp.config'.format(path),
+                           '{0}/configs/gwas_1KG_logistic.config'.format(path)])
+
+    with open('{0}/configs/temp.config'.format(path), 'w') as outfile:
+        with open('{0}/configs/gene_1KG_linear.config'.format(path), 'r') as infile:
+            for line in infile:
+                if line.lstrip().startswith('indir'):
+                    outfile.write('  indir  = "{0}/data"\n'.format(path))
+                elif line.lstrip().startswith('outdir'): 
+                    outfile.write('  outdir = "{0}/result-gene-1KG-linear"\n'.format(path))
+                else:
+                    outfile.write(line)
+
+    subprocess.call(['rm', '-rf', '{0}/configs/gene_1KG_linear.config'.format(path)])
+    subprocess.call(['mv', '{0}/configs/temp.config'.format(path),
+                           '{0}/configs/gene_1KG_linear.config'.format(path)])
+
+    with open('{0}/configs/temp.config'.format(path), 'w') as outfile:
+        with open('{0}/configs/gwla_1KG_linear_slope.config'.format(path), 'r') as infile:
+            for line in infile:
+                if line.lstrip().startswith('indir'):
+                    outfile.write('  indir  = "{0}/data"\n'.format(path))
+                elif line.lstrip().startswith('outdir'): 
+                    outfile.write('  outdir = "{0}/result-gwla-1KG-linear-slope"\n'.format(path))
+                else:
+                    outfile.write(line)
+
+    subprocess.call(['rm', '-rf', '{0}/configs/gwla_1KG_linear_slope.config'.format(path)])
+    subprocess.call(['mv', '{0}/configs/temp.config'.format(path),
+                           '{0}/configs/gwla_1KG_linear_slope.config'.format(path)])
+
     print('Initializing test data...')
     with open('{0}/data/toy_vcf.csv'.format(path), 'w') as outfile:
         for i in range(1, 23):
