19b3f484866137ff28e6d7a0b87d681d2025457b
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2022-01-12 13:04:17 -0500

    Update 05_combine_results.R

--- bin/05_combine_results.R
+++ bin/05_combine_results.R
@@ -7,10 +7,10 @@ suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(data.table))
 
 result <- fread(args[1], stringsAsFactors=F, header=T)
-for (i in 2:length(args)) {
-  res <- fread(args[i], stringsAsFactors=F, header=T)
-  result <- rbind(result, res)
-}
+try(for (i in 2:length(args)) {
+      res <- fread(args[i], stringsAsFactors=F, header=T)
+      result <- rbind(result, res)
+    })
 
 fwrite(result, paste0("all_chr_caf_annotated.csv"), row.names = FALSE)
 
