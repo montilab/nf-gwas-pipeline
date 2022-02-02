715ec4c356b18aa5f65d3526edcf1874bc0c6f5b
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2022-02-02 18:11:38 -0500

    Update 05_caf_by_group.R
    correct case and control numbers in data with missing genotypes

--- bin/05_caf_by_group.R
+++ bin/05_caf_by_group.R
@@ -26,66 +26,66 @@ analysis.sample.id <- readRDS(analysis.sample.id)
 pheno.dat <- pheno.dat[pheno.dat$sample.id%in%analysis.sample.id,]
 
 if(sum(colnames(pheno.dat)%in%c(group))==1){
-	table(pheno.dat[,group])
-	group_names <- names(table(pheno.dat[,group]))
-	out.caf <- c()
-	out.dosage <- c()
-	for(i in 1:length(group_names)){
-		group.id <- pheno.dat[pheno.dat[,group]==group_names[i],]$sample.id
-		seqSetFilter(gds, sample.id = group.id)
-		caf <- snpgdsSNPRateFreq(gds, sample.id = group.id)
-		caf <- 1-caf$AlleleFreq
-		if(dose=="true"){
-		  try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
-		  try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
-		}
-		seqResetFilter(gds)
-		out.caf <- cbind(out.caf, caf)
-		try(out.dosage <- cbind(out.dosage, dosage))
-	}
-	out.caf <- as.data.frame(out.caf)
-	try(out.dosage <- as.data.frame(out.dosage))
-	colnames(out.caf) <- paste0(group_names, ".caf")
-	try(colnames(out.dosage) <- paste0(group_names, ".dosage"))
-	if(dim(out.dosage)[1]==0){
-	    out.caf.dosage <- out.caf
-	}else{
-	    out.caf.dosage <- cbind(out.caf, out.dosage)
-	}
-	out.caf.dosage$chr <- seqGetData(gds,"chromosome")
-	out.caf.dosage$pos <- seqGetData(gds,"position")
+  table(pheno.dat[,group])
+  group_names <- names(table(pheno.dat[,group]))
+  out.caf <- c()
+  out.dosage <- c()
+  for(i in 1:length(group_names)){
+    group.id <- pheno.dat[pheno.dat[,group]==group_names[i],]$sample.id
+    seqSetFilter(gds, sample.id = group.id)
+    caf.dat <- snpgdsSNPRateFreq(gds, sample.id = group.id)
+    caf <- 1-caf.dat$AlleleFreq
+    if(dose=="true"){
+      try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
+      try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
+    }
+    seqResetFilter(gds)
+    out.caf <- cbind(out.caf, caf)
+    try(out.dosage <- cbind(out.dosage, dosage))
+  }
+  out.caf <- as.data.frame(out.caf)
+  try(out.dosage <- as.data.frame(out.dosage))
+  colnames(out.caf) <- paste0(group_names, ".caf")
+  try(colnames(out.dosage) <- paste0(group_names, ".dosage"))
+  if(dim(out.dosage)[1]==0){
+    out.caf.dosage <- out.caf
+  }else{
+    out.caf.dosage <- cbind(out.caf, out.dosage)
+  }
+  out.caf.dosage$chr <- seqGetData(gds,"chromosome")
+  out.caf.dosage$pos <- seqGetData(gds,"position")
 }else{
-	out.caf.dosage <- data.frame(chr=seqGetData(gds,"chromosome"), pos=seqGetData(gds,"position"))
-	print("group variable not found")
-	}
+  out.caf.dosage <- data.frame(chr=seqGetData(gds,"chromosome"), pos=seqGetData(gds,"position"))
+  print("group variable not found")
+}
 
 if (model == "logistic") {
-	case.id <- pheno.dat[pheno.dat[,phenotypes]==1,]$sample.id
-	seqSetFilter(gds, sample.id = case.id)
-	case.caf <- snpgdsSNPRateFreq(gds, sample.id = case.id)
-	case.caf <- 1-case.caf$AlleleFreq
-	if(dose=="true"){
-	  try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
-	  try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
-	}
-	seqResetFilter(gds)
-	
-	control.id <- pheno.dat[pheno.dat[,phenotypes]==0,]$sample.id
-	seqSetFilter(gds, sample.id = control.id)
-	control.caf <- snpgdsSNPRateFreq(gds, sample.id = control.id)
-	control.caf <- 1-control.caf$AlleleFreq
-	if(dose=="true"){
-	  try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
-	  try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
-	}
-	seqResetFilter(gds)
-
-	out.caf.dosage$n.case <- length(case.id)
-	out.caf.dosage$n.control <- length(control.id)
-	out.caf.dosage$case.caf <- case.caf
-	out.caf.dosage$control.caf <- control.caf
-	try(out.caf.dosage$case.dosage <- case.dosage)
-	try(out.caf.dosage$control.dosage <- control.dosage)
+  case.id <- pheno.dat[pheno.dat[,phenotypes]==1,]$sample.id
+  seqSetFilter(gds, sample.id = case.id)
+  case.caf.dat <- snpgdsSNPRateFreq(gds, sample.id = case.id)
+  case.caf <- 1-case.caf.dat$AlleleFreq
+  if(dose=="true"){
+    try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
+    try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
+  }
+  seqResetFilter(gds)
+  
+  control.id <- pheno.dat[pheno.dat[,phenotypes]==0,]$sample.id
+  seqSetFilter(gds, sample.id = control.id)
+  control.caf.dat <- snpgdsSNPRateFreq(gds, sample.id = control.id)
+  control.caf <- 1-control.caf.dat$AlleleFreq
+  if(dose=="true"){
+    try(dosage.dat <- seqGetData(gds, "annotation/format/DS")$data)
+    try(dosage <- apply(dosage.dat,2,sum)/dim(dosage.dat)[1]/2)
+  }
+  seqResetFilter(gds)
+  
+  out.caf.dosage$n.case <- round(length(case.id)*(1-case.caf.dat$MissingRate),0)
+  out.caf.dosage$n.control <- round(length(control.id)*(1-control.caf.dat$MissingRate),0)
+  out.caf.dosage$case.caf <- case.caf
+  out.caf.dosage$control.caf <- control.caf
+  try(out.caf.dosage$case.dosage <- case.dosage)
+  try(out.caf.dosage$control.dosage <- control.dosage)
 }
 
 
