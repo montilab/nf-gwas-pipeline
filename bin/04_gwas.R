6f32c057b61f037cd8932275c5188a777132afe3
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2022-03-30 19:17:47 -0400

    Update 04_gwas.R
    add missing rate filter

--- bin/04_gwas.R
+++ bin/04_gwas.R
@@ -4,10 +4,11 @@ gds.file <- args[1]
 annot1 <- args[2]
 annot2 <- args[3]
 nullmod <- args[4]
-test <- args[5]
-imputed <- args[6]
-result.file <- args[7]
-log.file <- args[8]
+max.miss <- as.numeric(args[5])
+test <- args[6]
+imputed <- args[7]
+result.file <- args[8]
+log.file <- args[9]
 
 sink(log.file, append=FALSE, split=TRUE)
 date()
@@ -32,14 +33,22 @@ seqData <- SeqVarData(gds, sampleData=annot)
 ####Null model
 nullmod <- readRDS(nullmod)
 
+####Calculate Missing Rate of SNPs
+seqSetFilter(seqData, sample.id=nullmod$fit$sample.id)
+snps$missing.rate <- seqMissing(seqData)
+
+snps.clean <- snps[snps$missing.rate < (1-max.miss), ]
+
 ####GWAS
+seqSetFilter(seqData, variant.id=snps.clean$variant.id)
+
 iterator <- SeqVarBlockIterator(seqData, verbose=FALSE)
 
 cat("\n####assocTestSingle starts\n")
 assoc <- assocTestSingle(iterator, nullmod, test=test, imputed=ifelse(imputed=="true",T,F), verbose=T)
 cat("####assocTestSingle ends\n\n")
 
-assoc <- left_join(assoc, snps, by="variant.id")
+assoc <- left_join(assoc, snps.clean, by="variant.id")
 colnames(assoc)[colnames(assoc)=="n.obs"] <- "N"
 
 fwrite(assoc, file = result.file, row.names=FALSE)
