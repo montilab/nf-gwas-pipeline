2d39403fda407ee57dfc24d9eb20c3daa71038d6
ZeyuanSong <51000742+ZeyuanSong@users.noreply.github.com>
2024-05-07 15:32:43 -0400

    Update 05_coincide_by_chr.R
    add missing.rate and MAC

--- bin/05_coincide_by_chr.R
+++ bin/05_coincide_by_chr.R
@@ -16,6 +16,7 @@ colnames(result.dat)[colnames(result.dat)=="CHR"] <- "chr"
 colnames(result.dat)[colnames(result.dat)=="POS"] <- "pos"
 colnames(result.dat)[colnames(result.dat)=="REF"] <- "ref"
 colnames(result.dat)[colnames(result.dat)=="ALT"] <- "alt"
+colnames(result.dat)[colnames(result.dat)=="MISSRATE"] <- "missing.rate"
 colnames(result.dat)[colnames(result.dat)=="SCORE"] <- "Score"
 colnames(result.dat)[colnames(result.dat)=="PVAL"] <- "Score.pval"
 colnames(result.dat)[colnames(result.dat)=="AF"] <- "freq"
@@ -23,6 +24,7 @@ colnames(result.dat)[colnames(result.dat)=="AF"] <- "freq"
 result.dat$Score.pval <- as.numeric(result.dat$Score.pval)
 result.dat$Score.SE <- sqrt(result.dat$VAR)
 result.dat$Score.Stat <- result.dat$Score/result.dat$Score.SE
+result.dat$MAC <- round( result.dat$N*2*ifelse(1-result.dat$freq < 0.5, 1-result.dat$freq, result.dat$freq))
 
 fwrite(result.dat, out.file, row.names=F)
 
